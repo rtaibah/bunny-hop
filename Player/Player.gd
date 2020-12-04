4ce2e37a3b88d1205fd88ca1da643039278b67a9
Rami Taibah <web+github@taibah.co>
2020-12-04 11:12:11 +0300

    Initial Commit

---
+++ Player/Player.gd
@@ -0,0 +1,51 @@
+extends KinematicBody2D
+
+var motion = Vector2(0,0)
+
+const SPEED = 1500
+const GRAVITY = 150
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 3500
+const WORLD_LIMIT = 3000
+
+signal animate
+
+func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP)
+	
+	
+func apply_gravity():
+	if position.y > WORLD_LIMIT:
+		game_over()
+	if  is_on_floor():
+		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = 1
+	else:
+		motion.y += GRAVITY
+		
+
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+		
+
+func move():
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
+		
+
+func animate():
+	emit_signal("animate", motion)
+#
+#
+func game_over():
+	get_tree().change_scene("res://Levels/GameOver.tscn")
