1481e996e9423dc4b2c6bfce9d6e177833e0ba76
Rami Taibah <web+github@taibah.co>
2020-12-05 10:57:47 +0300

    Added hurt function and sound effects/music

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,8 @@ const UP = Vector2(0,-1)
 const JUMP_SPEED = 3500
 const WORLD_LIMIT = 3000
 
+var lives = 3
+
 signal animate
 
 func _physics_process(delta):
@@ -32,7 +34,7 @@ func apply_gravity():
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
-		
+		$JumpSFX.play()
 
 func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
@@ -49,3 +51,16 @@ func animate():
 #
 func game_over():
 	get_tree().change_scene("res://Levels/GameOver.tscn")
+
+
+func hurt():
+	# Move up a pixel to bypass gravity
+	motion.y -= 1
+	# Stop doing anything for one frame only.
+	# So we are waiting for a signal from get_tree that is an "idle_frame"
+	yield(get_tree(), "idle_frame")
+	motion.y -= JUMP_SPEED
+	lives -= 1
+	$PainSFX.play()
+	if lives < 0:
+		game_over()
